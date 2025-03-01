384412a0bf52c1493b406003b9baf4e37b675c60
WinC3 <winstonchan333@gmail.com>
2025-03-01 18:50:17 -0500

    edited poly func gen

--- poly_model_func_generator.py
+++ poly_model_func_generator.py
@@ -11,10 +11,20 @@ degree = N
 combs = list(combinations_with_replacement(vars, degree))
 
 with open('poly_model_func.txt', 'w') as file:
+    # function header help
+    for var in vars:
+        file.write(var)
+        file.write(', ')
+    file.write('\n')
+    # combinations
+    i = 97 # ascii letter start
     for comb in combs:
+        file.write(chr(i))
+        file.write('*')
         for term in comb:
             file.write(term)
             file.write('*')
         file.write('1') # X1*X2*1, simpler code structure
         file.write('+')
+        i += 1
 
