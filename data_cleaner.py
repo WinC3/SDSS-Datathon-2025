c4f330ef49491b6561ac56aa38f7b3cda74a6d1b
WinC3 <winstonchan333@gmail.com>
2025-03-01 19:34:53 -0500

    cleaner update

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,4 +1,4 @@
-
+# prune data
 with open('real-estate-data.csv', 'r') as file:
     data = file.readlines()
 
@@ -13,3 +13,14 @@ with open('pruned-estate-data.csv', 'w') as file:
                 add = False
         if add:
             file.write(line)
+file.close()
+
+# split size and write to another file
+with open('pruned-estate-data.csv', 'r') as file:
+    data = file.readlines()
+
+file.close()
+
+with open('splitSize-estate-data.csv', 'w') as file:
+    for line in data:
+        elements = line.split(',')
